01034768d7d70cf69eeeabd063ee4174cfaa8771
Madhav Kanda <madhav.kanda@iitgn.ac.in>
2024-06-25 13:58:34 +0530

    Implemented Login

--- src/component/login/Login.js
+++ src/component/login/Login.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import { Link } from 'react-router-dom';
 import './Login.css'
+import {auth} from '../../firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 const Login = () =>{
+    const [email,setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const submitHandler = (e)=>{
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredentials)=>{
+            const user = userCredentials.user;
+            console.log(user)
+        })
+        .catch((error)=>{
+            console.log(error)
+        });
+    }
     return (
         <div className='login-wrapper'>
             <div className='login-container'>
@@ -11,10 +26,10 @@ const Login = () =>{
                 </div>
                 <div className = 'login-boxes login-right'>
                     <h2 className='login-heading'>Login</h2>
-                    <form>
-                        <input className='login-input' type='text' placeholder='Email'/>
-                        <input className='login-input' type='password' placeholder='Password'/>
-                        <input className='login-input login-btn' type='submit'/>
+                    <form onSubmit={submitHandler}>
+                    <input onChange={(e)=>{setEmail(e.target.value)}} className='login-input' type='text' placeholder='Email'/>
+                    <input onChange={(e)=>{setPassword(e.target.value)}} className='login-input' type='password' placeholder='Password'/>
+                    <input className='login-input login-btn' type='submit'/>
                     </form>
                     <Link to='/register' className='register-link'>Create an account</Link>
                 </div>
