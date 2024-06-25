dbfb325a8dedf936becb33fdf1a577870123c392
Madhav Kanda <madhav.kanda@iitgn.ac.in>
2024-06-25 15:03:43 +0530

    Completed basic ui for dashboard

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import '../../component/dashboard/dashboard.css'
+import {Link} from 'react-router-dom'
 
 const Dashboard = ()=>{
     return(
         <div className='dashboard-wrapper'>
             <div className='side-nav'>
-            
+                <div className='profile-info'>
+                    <img src={localStorage.getItem('photoURL')}/>
+                    <div>
+                    <p>{localStorage.getItem('cName')}</p>
+                    <button>logout</button>
+                    </div>
+                </div>
+                <hr/>
+                <div className='menu'>
+                <Link className='menu-link'> Home </Link>
+                <Link className='menu-link'> Invoices </Link>
+                <Link className='menu-link'> New Invoice </Link>
+                <Link className='menu-link'> Settings </Link>
+                </div>
             </div>
             <div className='main-container'>
                 
