c6258f1909e2e3862699f69a7a643c77d766bb9e
Madhav Kanda <madhav.kanda@iitgn.ac.in>
2024-07-04 16:23:18 +0530

    Completed Settings & Home (Before Adding Chart)

--- src/component/dashboard/Home.js
+++ src/component/dashboard/Home.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+    const [total, setTotal] = useState(123454)
+    const [totalInvoice, setTotalInvoice] = useState(2456)
+    const [totalMonthCollection, setTotalMonthCollection] = useState(4562)
     return (
         <div>
-            <p> Home</p>
+            <div className='home-first-row'>
+                <div className='home-box box-1'>
+                <h1>Rs {total}</h1>
+                <p>Overall</p>
+                </div>
+                <div className='home-box box-2'>
+                <h1>Rs {totalInvoice}</h1>
+                <p>Invoices</p>
+                </div>
+                <div className='home-box box-3'>
+                <h1>Rs {totalMonthCollection}</h1>
+                <p>This Month</p>
+                </div>
+            </div>
+            <div className='home-second-row'>
+                <div className='chart-box'>
+                    
+                </div>
+                <div className='recent-invoice-list'>
+
+                </div>
+            </div>
         </div>
     )
 }
--- src/component/dashboard/Setting.js
+++ src/component/dashboard/Setting.js
@@ -1,15 +1,35 @@
 import React, { useRef,useState } from 'react'
-import {storage} from '../../firebase'
+import {storage, auth, db} from '../../firebase'
 import {getDownloadURL, ref, uploadBytesResumable} from 'firebase/storage'
 import { updateProfile } from 'firebase/auth'
+import { doc, updateDoc } from 'firebase/firestore'
 
 const Setting = () => {
     const fileInputRef = useRef(null)
-    const [email,setEmail] = useState('')
+
+    const [email,setEmail] = useState(localStorage.getItem('email'))
     const [password, setPassword] = useState('')
     const [file, setfile] = useState(null)
-    const [displayName, setDisplayName] = useState('')
+    const [displayName, setDisplayName] = useState(localStorage.getItem('cName'))
     const [imageUrl, setImageUrl] = useState(localStorage.getItem('photoURL'))
+    const updateCompanyName = ()=>{
+        updateProfile(auth.currentUser,{
+            displayName:displayName
+        })
+        .then(res=>{
+            // window.location.reload()
+            localStorage.setItem('cName', displayName)
+            updateDoc(doc(db,"users", localStorage.getItem('uid')),{
+                displayName:displayName
+            })
+            .then(res=>{
+                window.location.reload()
+            })
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
     const onSelectFile = (e)=>{
         setfile(e.target.files[0])
         setImageUrl(URL.createObjectURL(e.target.files[0]))
@@ -29,10 +49,14 @@ const Setting = () => {
         <div>
             <p> Setting</p>
             <div className='setting-wrapper'>
-                <div className='profile-info'>
+                <div className='profile-info update-cName'>
                     <img onClick={()=>{fileInputRef.current.click()}} className='pro' alt='profile-pic' src={imageUrl}/>
                     <input onChange={(e)=>{onSelectFile(e)}} style={{display:'none'}} type='file' ref={fileInputRef}/>
-                    {file && <button onClick={()=>{updateLogo()}}>Update Profile Pic</button>}
+                    {file && <button onClick={()=>{updateLogo()}} style={{width:'30%', padding:'10px', backgroundColor:'hotpink'}}>Update Profile Pic</button>}
+                </div>
+                <div className='update-cName'>
+                    <input onChange={e=>{setDisplayName(e.target.value)}} type='text' placeholder='Company Name' value={displayName} />
+                    <button onClick={updateCompanyName}>Update Company Name</button>
                 </div>
             </div>
         </div>
